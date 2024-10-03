174ec3790a7d2a9eb1d0c53749fa3997ddc24e8a
fatimaabubakaradam <zahrammcy@gmail.com>
2024-10-03 12:33:23 -0700

    add javascript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const closeMenu = document.getElementById('closeMenu');
+    const navMenu = document.getElementById('navMenu');
+
+    if (menuToggle && navMenu && closeMenu) {
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+
+        closeMenu.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    }
+}); 
 document.addEventListener('DOMContentLoaded', function() {
     function renderWhySection() {
         const section = document.createElement('section');
